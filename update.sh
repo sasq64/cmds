a875529477f9808b2746897dc5c17ccc72ae6266
Jonas Minnberg <sasq64@gmail.com>
2015-11-24 18:56:35 +0100

    Some more files

---
+++ update.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+wget http://amigaremix.com/exporter -O amiremix.txt
+wget http://chipmachine:chipmusikmaskine@remix.kwed.org/external/sidfind/rko2chipmachine.csv.gz -O rko.txt.gz
+gunzip rko.txt.gz
+wget http://csdb.dk/misc/releases_with_hvsc.php -O csdb.xml
+
+wget ftp://ftp.modland.com/allmods.zip
